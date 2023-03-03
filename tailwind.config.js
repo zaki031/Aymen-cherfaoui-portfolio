f32bfc872199161be0a2f41e00a1efb02abfc6d9
zaki031 <115791170+zaki031@users.noreply.github.com>
2023-03-03 12:36:02 +0100

    home page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+
+  theme: {
+    extend: {
+      colors:{
+                "bg":"#161616",
+    }
+    },
+  },
+  plugins: [],
+}
